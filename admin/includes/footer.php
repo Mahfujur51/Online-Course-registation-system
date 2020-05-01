7563529f89aae448a2d00a990f755417e65667b0
Mahfujur51 <overcastmoon@gmail.com>
2020-05-01 08:15:42 +0600

    initial admin templet setup

---
+++ admin/includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php  echo Date('Y'); ?> Online Course Registration | By : <a href="www.facebook.com/mahfujur.just" target="_blank">
+                    	Mahfujur
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
